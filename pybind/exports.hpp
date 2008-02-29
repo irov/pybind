7e9e049c3666f53e4c2475e5f898046039c60fe5
yuriy_levchenko <yuriy_levchenko@fb565425-9256-4457-912b-8b7c3a94f18b>
2008-02-29 16:15:50 +0000

    INTERNAL* refactoring solution

--- pybind/exports.hpp
+++
@@ -1,9 +0,0 @@
-#	pragma once
-
-#	ifdef PYBIND_EXPORTS
-#	define PYBIND_API __declspec(dllexport)
-#	else
-#	define PYBIND_API __declspec(dllimport)
-#	endif
-
-#	pragma warning ( disable : 4251 ) // needs to have dll-interface to be used by clients of class
--- pybind/pybind.hpp
+++
@@ -1,8 +0,0 @@
-#	pragma once
-
-#	include "pybind/types.hpp"
-#	include "pybind/extract.hpp"
-
-#	include "pybind/def.hpp"
-#	include "pybind/class.hpp"
-#	include "pybind/system.hpp"
