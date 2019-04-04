42f26fd426a6643c2129af9b0e808e51bbfdfaf1
irov <irov13@mail.ru>
2019-04-04 14:39:03 +0300

    update intrusive ptr fix collect

--- src/python/python_kernel.cpp
+++ src/python/python_kernel.cpp
@@ -129,6 +129,7 @@ namespace pybind
     //////////////////////////////////////////////////////////////////////////
     void python_kernel::collect()
     {
+        PyImport_Cleanup();
         PyType_ClearCache();
         PyGC_Collect();
     }
